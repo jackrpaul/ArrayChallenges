c98fcf9e57c22c22f3acf756b6cc4afe0782f0fb
Jack <jackrpaul13@gmail.com>
2017-09-11 16:28:15 -0600

    finished challenges 1 and 2

--- Array Challenges/ArrayChallenges.java
+++ Array Challenges/ArrayChallenges.java
@@ -1,9 +1,10 @@
-
-public class ArrayChallenges {
-
-	public static void main(String[] args) 
+import java.util.Scanner;
+public class ArrayChallenges 
+{
+static boolean moreSweets;
+	public static void main(String[] args)
 	{
-		
+	moreSweets = true;
 	challenge1();
 	challenge2();
 	challenge3();
@@ -12,8 +13,16 @@ public class ArrayChallenges {
 
 	private static void challenge1() 
 	{
-		
-		
+	String [] sweets;
+	Scanner userInput = new Scanner(System.in);
+	System.out.println("How many different sweets do you have?");
+	int numberOfSweets = userInput.nextInt();
+	sweets = new String [numberOfSweets];
+	for (int i = 0; i < numberOfSweets; i++)
+		{
+		System.out.println("Tell me the name of a sweet you like.");
+		String sweetName = userInput.nextLine();
+		}
 	}
 
 	private static void challenge2() 
--- Array Challenges/src/ArrayChallenges.java
+++ Array Challenges/src/ArrayChallenges.java
@@ -1,9 +1,68 @@
+import java.util.Scanner;
 
-public class ArrayChallenges {
+public class ArrayChallenges 
+{
+static int evenSum, oddSum;
+	public static void main(String[] args)
+	{
+		
+	//challenge1();
+	//challenge2();
+	challenge3();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
 
+	private static void challenge1() 
+	{
+	String [] sweets;
+	Scanner userSweet = new Scanner(System.in);
+	Scanner userInput = new Scanner(System.in);
+	System.out.println("How many different sweets do you have?");
+	sweets = new String [userSweet.nextInt()];
+	for (int i = 0; i < sweets.length; i++)
+		{
+		System.out.println("Tell me the name of a sweet you like.");
+		String sweetName = userInput.nextLine();
+		sweets [i] = sweetName;
+		}
+	System.out.print("Oh wow, I also like ");
+	for (int i = 0; i < sweets.length; i++)
+		{
+		if (i == sweets.length - 1)
+			{
+			System.out.print("and " + sweets[i] + "!");
+			}
+		else
+			{
+			System.out.print(sweets [i] + ", ");
+			}
+		}
+	}
+
+	private static void challenge2() 
+	{
+	int [] octaveArray = new int [8];
+	Scanner userNumber = new Scanner(System.in);
+	System.out.println("Please give me 8 numbers.");
+	for (int i = 0; i < octaveArray.length; i++)
+		{
+		octaveArray [i] = userNumber.nextInt();
+		if (octaveArray [i] % 2 == 0)
+			{
+			evenSum += octaveArray [i];
+			}	
+		else
+			{
+			oddSum += octaveArray [i];
+			}
+		}
+	System.out.println("The sum of the even numbers in this array is " + evenSum + ", and the sum of the odd numbers in this array is " + oddSum + ".");
+	}
+
+	private static void challenge3() 
+	{
+	
+		
 	}
 
 }
