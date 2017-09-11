9ff0525885710b3a1f8252c3a0a40cef97b7cd62
Jack <jackrpaul13@gmail.com>
2017-09-11 15:59:00 -0600

    initial commit

---
+++ Array Challenges/ArrayChallenges.java
@@ -0,0 +1,31 @@
+
+public class ArrayChallenges {
+
+	public static void main(String[] args) 
+	{
+		
+	challenge1();
+	challenge2();
+	challenge3();
+
+	}
+
+	private static void challenge1() 
+	{
+		
+		
+	}
+
+	private static void challenge2() 
+	{
+		
+		
+	}
+
+	private static void challenge3() 
+	{
+		
+		
+	}
+
+}
